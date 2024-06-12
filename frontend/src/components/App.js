b2833f10ce305e2650189866146e2abd3077e80e
Arsen H <77290710+arsenhh-byte@users.noreply.github.com>
2024-06-12 12:13:55 +0300

    images are live!!

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, IconButton, Grid } from '@mui/material';
+import { Container, Typography, IconButton, Grid, Box } from '@mui/material';
 import SearchBar from '../components/SearchBar';
 import SearchResult from '../components/SearchResult';
 import ReadingList from '../components/ReadingList';
 import AddIcon from '@mui/icons-material/Add';
+import image1 from "../assets/image1.webp";
+import image2 from "../assets/image2.webp";
+import image3 from "../assets/image3.webp";
+import image4 from "../assets/image4.webp";
+import image5 from "../assets/image5.webp";
+import image6 from "../assets/image6.webp";
+import image7 from "../assets/image7.webp";
+import image8 from "../assets/image8.webp";
+import image9 from "../assets/image9.webp";
+import image10 from "../assets/image10.webp";
 import './App.css';
+
+const images = {
+  'image1': image1,
+  'image2': image2,
+  'image3': image3,
+  'image4': image4,
+  'image5': image5,
+  'image6': image6,
+  'image7': image7,
+  'image8': image8,
+  'image9': image9,
+  'image10': image10,
+};
 
 const App = () => {
   // State to store the search query entered by the user
@@ -38,7 +61,7 @@ const App = () => {
         // Prepend the server URL to the coverPhotoURL
         const booksWithFullURL = result.data.books.map(book => ({
           ...book,
-          coverPhotoURL: `http://localhost:4000/assets/${book.coverPhotoURL}`
+          coverPhotoURL: `${book.coverPhotoURL}`
         }));
         setBooks(booksWithFullURL);
       } catch (error) {
@@ -73,7 +96,7 @@ const App = () => {
         Teacher's Book Assignment Tool
       </Typography>
       <Typography variant="body1" gutterBottom>
-        Welcome to your book assignment tool. This tool is meant to help you figure out which book would be the best assignment for your students. 
+        Welcome to your book assignment tool. This tool is meant to help you figure out which book would be the best assignment for your students.
         ENJOY :)
       </Typography>
       <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
@@ -89,17 +112,22 @@ const App = () => {
       <Typography variant="h5" gutterBottom>
         All Books
       </Typography>
-      <Grid container spacing={3} className="image-grid">
+      <Grid container spacing={1} className="image-grid">
         {books.map((book, index) => (
           <Grid item xs={12} sm={6} md={4} lg={3} key={index} className="grid-item">
-            <img src={book.coverPhotoURL} alt={book.title} />
-            <Typography variant="body1">{book.title}</Typography>
-            <Typography variant="body2">{book.author}</Typography>
-            <Typography variant="body3">{book.readingList}</Typography>
-
-            <IconButton onClick={() => addBook(book)} className="addButton">
-              <AddIcon />
-            </IconButton>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+              <img src={images[book.coverPhotoURL.split('/')[1].split('.')[0]]} alt={book.title} className="image-resize" />
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '8px' }}>
+              <Typography variant="body1">{book.title}</Typography>
+              <Typography variant="body3">{book.author}</Typography>
+              <Typography variant="body2">{book.readingLevel}</Typography>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <IconButton onClick={() => addBook(book)} className="addButton">
+                <AddIcon />
+              </IconButton>
+            </div>
           </Grid>
         ))}
       </Grid>
--- frontend/src/components/ReadingList.js
+++ frontend/src/components/ReadingList.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import { List, ListItem, ListItemText, IconButton, Avatar, Typography, Box } from '@mui/material';
+import { List, ListItem, ListItemText, IconButton, Typography, Box } from '@mui/material';
 import RemoveIcon from '@mui/icons-material/Remove';
+import image1 from "../assets/image1.webp";
+import image2 from "../assets/image2.webp";
+import image3 from "../assets/image3.webp";
+import image4 from "../assets/image4.webp";
+import image5 from "../assets/image5.webp";
+import image6 from "../assets/image6.webp";
+import image7 from "../assets/image7.webp";
+import image8 from "../assets/image8.webp";
+import image9 from "../assets/image9.webp";
+import image10 from "../assets/image10.webp";
+
+const images = {
+  'image1': image1,
+  'image2': image2,
+  'image3': image3,
+  'image4': image4,
+  'image5': image5,
+  'image6': image6,
+  'image7': image7,
+  'image8': image8,
+  'image9': image9,
+  'image10': image10,
+};
 
 const ReadingList = ({ readingList, removeBook }) => {
   return (
@@ -11,15 +34,22 @@ const ReadingList = ({ readingList, removeBook }) => {
         </Typography>
       ) : (
         <List>
-          {readingList.map((book) => (
-            <ListItem key={book.title} alignItems="flex-start" className="readingListItem">
-              <Avatar src={book.coverPhotoURL} variant="square" className="readingListAvatar" />
-              <ListItemText primary={book.title} secondary={book.author} />
-              <IconButton onClick={() => removeBook(book.title)}>
-                <RemoveIcon />
-              </IconButton>
-            </ListItem>
-          ))}
+          {readingList.map((book) => {
+            const imageName = book.coverPhotoURL.split('/').pop().split('.')[0];
+            return (
+              <ListItem key={book.title} alignItems="flex-start" className="readingListItem">
+                <img src={images[imageName]} alt={book.title} className="readingListImage" />
+                <ListItemText 
+                  primary={book.title} 
+                  secondary={book.author} 
+                  className="readingListText"
+                />
+                <IconButton onClick={() => removeBook(book.title)}>
+                  <RemoveIcon />
+                </IconButton>
+              </ListItem>
+            );
+          })}
         </List>
       )}
     </Box>
